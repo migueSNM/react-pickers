5d112a01875a3d2a718eca375233f1abc3fe70dc
mchirinos <miguel.chirinos@quadminds.com>
2019-02-26 12:35:16 -0300

    reorder component files and create task template picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import './components/TaskPicker'
+import './components/TaskPicker/TaskPicker'
 import TaskPicker from "./components/TaskPicker";
+import TaskTemplatePicker from "./components/TaskTemplatePicker";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TaskPicker/>
+        <TaskTemplatePicker/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from "apollo-boost";
-import gql from "graphql-tag";
+import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from "react-apollo";
 import './index.css';
 import App from './App';
