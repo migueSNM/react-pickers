ad38afb468f849c76afd8f8728b480b70f54a2f2
mchirinos <miguel.chirinos@quadminds.com>
2019-03-13 16:45:44 -0300

    filters deleted and type

--- src/components/TaskPicker/TaskPicker.jsx
+++ src/components/TaskPicker/TaskPicker.jsx
@@ -4,7 +4,6 @@ import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -18,21 +17,29 @@ import SearchIcon from '@material-ui/icons/Search';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import FilterListIcon from '@material-ui/icons/FilterList';
+import Chip from '@material-ui/core/Chip';
+import DoneIcon from '@material-ui/icons/Done';
 import SortIcon from '@material-ui/icons/Sort';
-import CodeIcon from '@material-ui/icons/Code';
-import LabelIcon from '@material-ui/icons/Label';
-import FilterNoneIcon from '@material-ui/icons/FilterNone';
-import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
 import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import Badge from '@material-ui/core/Badge';
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import { omit, groupBy, mapObject } from 'underscore';
 
 
 const styles = {
   drawer: {
     height: '100%',
+  },
+  counterChip: {
+    margin: '0 10px',
+    backgroundColor: '#FFFFFF',
+    fontSize: 15,
   },
   taskList: {
     width: '100%',
@@ -109,10 +116,24 @@ class TaskPicker extends React.Component {
     bottom: false,
     rightDrawer: false,
     checked: [],
+    itemsFiltered: [],
     applied: [],
     searchText: '',
     rightDrawerValue: 0,
     sortBy: 'code',
+    filterNestedOpen : {
+      status: false,
+      type: false,
+      group: false,
+    },
+    filterChecked: {
+      deleted: false,
+      type: []
+    },
+    filterApplied: {
+      deleted: false,
+      type: []
+    },
   };
 
   toggleDrawer = (side, open) => () => {
@@ -123,10 +144,77 @@ class TaskPicker extends React.Component {
   };
 
   toggleRightDrawer = () => {
-
     this.setState({
       rightDrawer: !this.state.rightDrawer,
     });
+  };
+
+  handleOpenFilterOption = event => {
+    const columnName = event.target.innerText;
+    const newFilterNestedOpen = {...this.state.filterNestedOpen};
+    newFilterNestedOpen[columnName] = !newFilterNestedOpen[columnName];
+    this.setState({
+      filterNestedOpen: newFilterNestedOpen,
+    });
+  };
+
+  handleSelectFilterOption = (columnValue, columnName) => () => {
+
+    const { filterChecked } = this.state;
+    const currentIndex = filterChecked.map(function(e) { return e.value; }).indexOf(columnValue);
+    const newFilterChecked = [...filterChecked];
+
+    if (currentIndex === -1) {
+      newFilterChecked.push({
+        colName: columnName,
+        value: columnValue
+      });
+    } else {
+      newFilterChecked.splice(currentIndex, 1);
+    }
+
+    this.setState({
+      filterChecked: newFilterChecked,
+    });
+  };
+
+  handleSelectFilterDeleted = () => {
+    const newFilterChecked = {...this.state.filterChecked};
+    newFilterChecked.deleted = !this.state.filterChecked.deleted;
+    this.setState({
+      filterChecked: newFilterChecked,
+    });
+  };
+
+  handleApplyFilter = () => {
+    const { filterChecked } = this.state;
+    const filtersToApply = {...filterChecked};
+
+    // const groupedFilters = mapObject(groupBy(filtersToApply, 'colName'),
+    //   clist => clist.map(filter => omit(filter, 'colName')));
+
+    this.setState({
+      filterApplied: filtersToApply,
+    });
+  };
+
+  handleCleanFilter = () => {
+    this.setState({
+      filterChecked: {
+        deleted: false,
+        type: [],
+      },
+      filterApplied: {
+        deleted: false,
+        type: [],
+      },
+    });
+  };
+
+  handleFilterBadge = () => {
+    const { deleted, type } = this.state.filterApplied;
+
+    return (!deleted && !type.length);
   };
 
 
@@ -186,15 +274,16 @@ class TaskPicker extends React.Component {
     });
   };
 
-  handleApply = () => {
+  handleConfirm = () => {
     const checkedIds = this.state.checked;
-    this.props.handleReturnedIds(checkedIds)
+    this.props.handleReturnedIds(checkedIds);
+    //TODO cerrar el picker cuando se confirmen los tasks
   };
 
   render() {
     const { classes } = this.props;
     return (
-      <div>
+      <div className={classes.root}>
         <Button onClick={this.toggleDrawer('bottom', true)}>Task Picker</Button>
         <Drawer
           anchor="bottom"
@@ -217,6 +306,13 @@ class TaskPicker extends React.Component {
                     type {
                       name
                     }
+                    groups {
+                      name
+                      code
+                    }
+                    audit {
+                      _deletedAt
+                    }
                   }
                 }
               }
@@ -225,6 +321,12 @@ class TaskPicker extends React.Component {
             {({ loading, error, data }) => {
               if (loading) return <p>Loading...</p>;
               if (error) return <p>Error :(</p>;
+
+              //  these distinct values are use to show the available values to filter
+              // let distinctValues = {};
+              // Object.keys(data.tasks.docs[0]).forEach( value => {
+              //   distinctValues[value] = [...new Set(data.tasks.docs.map(task => task[value]))]
+              // });
 
               return (
               <Fragment>
@@ -236,23 +338,27 @@ class TaskPicker extends React.Component {
                       </IconButton>
                       <Typography variant="h6" color="inherit" className={classes.grow}>
                         Tasks
+                        <Chip label={this.state.checked.length} className={classes.counterChip}/>
                       </Typography>
                       <div className={classes.searchBox}>
                         <SearchIcon />
                         <InputBase
                           placeholder="Search…"
                           className={classes.searchBoxText}
-                          onChange={this.handleSearchChange()
-                          }
+                          onChange={this.handleSearchChange()}
                         />
                       </div>
                       <IconButton onClick={this.toggleRightDrawer} className={classes.sortButton} aria-label="Sort">
                         <SortIcon />
                       </IconButton>
                       <IconButton onClick={this.toggleRightDrawer} className={classes.filterButton} aria-label="Filter">
-                        <FilterListIcon />
+                        <Badge color="secondary" variant="dot" invisible={this.handleFilterBadge()}>
+                          <FilterListIcon />
+                        </Badge>
                       </IconButton>
-                      <Button onClick={this.handleApply} color="inherit">Apply</Button>
+                      <IconButton onClick={this.handleConfirm} color="inherit" aria-label="Confirm">
+                        <DoneIcon />
+                      </IconButton>
                     </Toolbar>
                   </AppBar>
                   <ListItem role={undefined} dense className={classes.taskListItem}>
@@ -273,6 +379,13 @@ class TaskPicker extends React.Component {
                   <Divider variant="middle" />
 
                   {data.tasks.docs
+                    .filter(task => {
+                      if(this.state.filterApplied.deleted){
+                        return task.audit._deletedAt
+                      } else {
+                        return true
+                      }
+                    })
                     .filter(task => Object.entries(task).some(val =>
                         val[0] !== '_id' && compareStringCaseInsensitive(val[1], this.state.searchText)
                     ))
@@ -318,26 +431,53 @@ class TaskPicker extends React.Component {
                     <Tab label="Filter" />
                   </Tabs>
                   {this.state.rightDrawerValue === 0 && (
-                    <List
-                      component="nav"
-                      subheader={<ListSubheader component="div">Sort by</ListSubheader>}
-                    >
+                    <List component="nav">
                       {Object.keys(data.tasks.docs[0]).map(column =>
                         !column.startsWith('_') &&
                         (
                           <ListItem button key={column} onClick={this.handleSort(column)}>
                             <ListItemIcon>
-                              {column === 'code' ? <CodeIcon /> : ''}
-                              {column === 'name' ? <LabelIcon /> : ''}
-                              {column === 'type' ? <FilterNoneIcon /> : ''}
-                              {column === 'price' ? <AttachMoneyIcon /> : ''}
-                              {column === 'description' ? <LibraryBooksIcon /> : ''}
+                              <LibraryBooksIcon />
                             </ListItemIcon>
                             <ListItemText primary={column}/>
                           </ListItem>
                         ))
                       }
                     </List>
+                  )}
+                  {this.state.rightDrawerValue === 1 && (
+                    <Fragment>
+                      <List component="nav">
+                        <ListItem button key="status" onClick={this.handleOpenFilterOption}>
+                          <ListItemText inset primary="status" />
+                          {this.state.filterNestedOpen.status ? <ExpandLess /> : <ExpandMore />}
+                        </ListItem>
+                        <Collapse in={this.state.filterNestedOpen.status} timeout="auto" unmountOnExit>
+                          <ListItem
+                            key="deleted"
+                            role={undefined}
+                            dense
+                            button
+                            onClick={this.handleSelectFilterDeleted}
+                            className={classes.taskListItem}
+                          >
+                            <Checkbox
+                              checked={this.state.filterChecked.deleted}
+                              tabIndex={-1}
+                              disableRipple
+                              classes={{
+                                root: classes.checkbox,
+                                checked: classes.checked,
+                              }}
+                            />
+                            <ListItemText className={classes.taskListItemText} primary='Show deleted' />
+                          </ListItem>
+                        </Collapse>
+                      </List>
+                      <Button onClick={this.handleApplyFilter} color="inherit">Apply Filters</Button>
+                      <Button onClick={this.handleCleanFilter} color="inherit">Clean Filters</Button>
+                    </Fragment>
+
                   )}
                 </Drawer>
               </Fragment>
